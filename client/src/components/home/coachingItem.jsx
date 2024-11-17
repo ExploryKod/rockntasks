a321440589019ca3cc2b5965d7a91c9f9261d04c
Amaury Franssen <amauryfranssen@gmail.com>
2024-11-17 18:58:01 +0100

    chore: suppress unused code

--- client/src/components/home/coachingItem.jsx
+++ client/src/components/home/coachingItem.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import image_1 from "../../assets/img/categories/proteines.jpeg";
+import image_1 from "../../assets/img/activities/sport.jpeg";
 import image_2 from "../../assets/img/categories/vegetables.jpeg";
-import image_3 from "../../assets/img/categories/fruits.jpg";
-import image_4 from "../../assets/img/categories/spices.jpeg";
-import image_5 from "../../assets/img/categories/feculents.jpeg";
-import image_6 from "../../assets/img/categories/beverage.jpeg";
-import image_7 from "../../assets/img/categories/recipes.jpg";
+
 
 const images = [
     { imageUrl: image_1, category_id: 1 },
-    { imageUrl: image_2, category_id: 2 },
-    { imageUrl: image_3, category_id: 3 },
-    { imageUrl: image_4, category_id: 4 },
-    { imageUrl: image_5, category_id: 5 },
-    { imageUrl: image_6, category_id: 6 },
-    { imageUrl: image_7, category_id: 7 },
+    { imageUrl: image_2, category_id: 2 }
 ]
 const CoachingItem = ({ category }) => {
   // We need image url so we pass it through props (here ou directly in parenthesis of the function)
