d39d352b980f80143b82a477f5f778db050bba29
Amaury Franssen <amauryfranssen@gmail.com>
2024-11-17 19:11:36 +0100

    chore: suppress unused code

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -5,6 +5,15 @@ const Food = () => {
 
   return (
     <div className="home">
+     <div className="categories-container">
+        <h1 className="category-title">Bienvenue sur votre site de coaching !</h1>
+        <p  className="category-text">Ce site est un site-relai entre foodcare et vous même. Votre coach a choisi pour vous une palette de tâches et des aliments sains. 
+            Commandez certains aliments et engagez-vous sur certaines tâches.
+            Vous pouvez en outre acheter directement des aliments à notre boutique si cela facilite vos engagements. 
+            Ils seront disponible pour vous dans l'une de nos 260 boutiques en Europe.
+            Une fois vos commandes et engagements passé, votre coach vous contacte pour la suite des évènements.
+            </p>     
+    </div>
     <HomeContainers/>
     <Outlet/>
     </div>
