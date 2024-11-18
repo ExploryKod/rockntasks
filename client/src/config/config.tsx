d4c7ef1a45193cfa07c4afbde294694f7598349a
Amaury Franssen <amauryfranssen@gmail.com>
2024-11-18 22:57:47 +0100

    try to fox build

--- client/src/config/config.tsx
+++ client/src/config/config.tsx
@@ -2,7 +2,7 @@ interface Config {
   serverPort: string | number;
   serverHost: string;
 }
-
+console.info("server host is : ", process.env.VITE_SITE_URL_HTTP )
 const config:Config = {
     serverPort: 8000,
     serverHost: process.env.VITE_SITE_URL_HTTP ?? "https://rockntasks-api.onrender.com/"
